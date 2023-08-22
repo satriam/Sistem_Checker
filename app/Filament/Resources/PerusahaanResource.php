4680a895cac7e87ca3cee8c61c13432d2db3bac9
satriam <satriabae43@gmail.com>
2023-08-22 12:47:37 +0700

    first commit

---
+++ app/Filament/Resources/PerusahaanResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PerusahaanResource\Pages;
+use App\Filament\Resources\PerusahaanResource\RelationManagers;
+use App\Models\Perusahaan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PerusahaanResource extends Resource
+{
+    protected static ?string $model = Perusahaan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                 Forms\Components\Card::make()
+                ->schema([
+                     Forms\Components\TextInput::make('nama_perusahaan'),
+                     Forms\Components\TextInput::make('alamat')
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama_perusahaan')
+                >searchable()
+        ->sortable() ,
+                Tables\Columns\TextColumn::make('alamat')
+                ->searchable()
+        ->sortable()
+                ->visibleFrom('md')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManagePerusahaans::route('/'),
+        ];
+    }    
+}
---
+++ database/migrations/2023_08_22_032409_create_karyawans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_karyawan');
+            $table->integer('nomor_pegawai')->unique();
+            $table->unsignedBigInteger('perusahaan_id');
+
+            $table->foreign('perusahaan_id')->references('id')->on('perusahaans')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};
